eb819a98347867f4170e45b475c6ed1945c716d7
mani <itsmanikandanraju@gmail.com>
2025-07-19 21:55:22 +0530

    restore factorial generator implementation

--- problems/factorial_generator.py
+++ problems/factorial_generator.py
@@ -5,23 +5,24 @@ def fact_gen():
     i = 0
     fact = 1
     while True:
-        if i <= 1:
+        if i == 0:
             yield 1
         else:
-            yield i * fact
             fact *= i
+            yield fact
         i += 1
 
 
 def fact(n):
-    if n <=1:
+    if n <= 1:
         return 1
     else:
         return fact(n-1) * n
 
 
-for i,v in enumerate(fact_gen()):
-    print(i,v)
+
+for i, v in zip(range(10), fact_gen()):
+    print(i, v)
     time.sleep(0.5)
-    print(i,fact(i))
+    print(i, fact(i))
     
